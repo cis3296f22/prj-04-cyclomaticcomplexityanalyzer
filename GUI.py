d93723c2f13a9579ce8860529e0e2eb29280ce63
kroos <Kroosvi@gmail.com>
2022-11-13 15:45:11 -0500

    "GUI welcome done"

--- GUI.py
+++ GUI.py
@@ -1,38 +1,51 @@
 import tkinter as tk
-from tkinter.filedialog import askopenfilename
-
-# GUI starts here
-root = tk.Tk()
+from tkinter import Entry, Label
 
 
-# when user click on the browse button it will take them here
-# as soon as they click the browse button it will change to loading
-def open_file() -> str:
-    browse_text.set("Loading")
-    file = askopenfilename(parent=root, title='choose a file')
+def start_gui():
+    # GUI starts here
+    root = tk.Tk()
+    root.title("Cyclomatic Complexity Analyzer")
 
-    if file:
-        print("file successfully loaded")
-        browse_text.set("Browse")
+    def error_invaild(label):
+        label.set("Please enter a vaild Github URL")
 
-    return file
+    # when user click on the browse button it will take them here
+    # as soon as they click the browse button it will change to loading
+    def open_file() -> str:
+        submitt_text.set("Loading...")
+        url = input_box.get()
 
+        # Given GitHub URL is not vaild then we prompt the user NEED TO TEST
+        # if not check_github_url(url):
+        #    error_invaild(instraction)
 
-# background page
-canvas = tk.Canvas(root, width=600, height=400)
-canvas.grid(columnspan=3)
+        # TODO: call the backend here!!!
 
-# instruction
-instruction = tk.Label(root, text="Click Browse to select the project that you want to analyze")
-instruction.grid(columnspan=3, column=0, row=1)
+        submitt_text.set("Enter")
 
-# browse button -> when user loaded in the file it will say loading not browse
-browse_text = tk.StringVar()
-browse_button = tk.Button(root, command=lambda: open_file(), textvariable=browse_text)
+    # background page
+    canvas = tk.Canvas(root, width=600, height=400)
+    canvas.grid(columnspan=5)
 
-# setting the initial text in the button
-browse_text.set("Browse")
-browse_button.grid(column=1, row=2)
+    # instraction
+    instraction = tk.StringVar()
+    instraction.set("To start please enter a vaild GitHub URL")
 
-# end of the GUI
-root.mainloop()
+    instraction_label = Label(textvariable=instraction)
+    instraction_label.grid(column=1, row=0)
+
+    # input box
+    input_box = Entry(root, borderwidth=5)
+    input_box.grid(column=0, row=1)
+
+    # setting the initial text in the button
+    submitt_text = tk.StringVar()
+    submitt_text.set("Enter")
+
+    # browse button -> when user loaded in the file it will say loading not browse
+    browse_button = tk.Button(root, command=lambda: open_file(), textvariable=submitt_text)
+    browse_button.grid(column=1, row=2)
+
+    # end of the GUI
+    root.mainloop()
