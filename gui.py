d0ff2f93b787bf00a43e05ff9e6de31bce8eeb76
Patrick Ammons <112198910+tuk45984@users.noreply.github.com>
2022-11-21 23:59:37 -0500

    Add a CCN histogram to the details tab

--- gui.py
+++ gui.py
@@ -1,11 +1,14 @@
 import dataclasses
+import enum
 import subprocess
 from typing import Optional, Union
-import enum
 
 import dearpygui.dearpygui as dpg
+import pandas as pd
+import numpy as np
 
 import analysis_api
+import math
 
 Id = Union[int, str]
 
@@ -77,8 +80,7 @@ def start() -> Id:
             return
         sort_specs = [(PRETTY_DETAILS_COLUMNS[dpg.get_item_label(x)].raw_name, y == 1) for x, y in sort_specs]
         [sort, ascending] = zip(*sort_specs)
-        tab = dpg.get_item_parent(tbl)
-        file_path = dpg.get_item_user_data(tab)
+        file_path = dpg.get_item_user_data(tbl)
         [_, file_name] = file_path.rsplit('\\', 1)
         per_file = repo.analyze_files(
             file_filter=lambda df: df[df.file_name == file_name],
@@ -115,9 +117,50 @@ def start() -> Id:
                 continue
             [_, file_name] = file.rsplit('\\', 1)
 
-            with dpg.tab(label=f"{file_name}", parent=data_tab_bar, user_data=file):
-                with dpg.table(callback=sort_details_callback, sortable=True, sort_multi=True, policy=dpg.mvTable_SizingStretchProp) as tbl:
-                    write_table(tbl, df, DETAILS_COLUMNS)
+            with dpg.tab(label=file_name, parent=data_tab_bar, user_data=file):
+                with dpg.group(horizontal=False):
+                    with dpg.table(callback=sort_details_callback, sortable=True, sort_multi=True,
+                                   policy=dpg.mvTable_SizingStretchProp, user_data=file, scrollY=True,
+                                   height=400, width=-1) as tbl:
+                        write_table(tbl, df, DETAILS_COLUMNS)
+                    X, Y = make_histogram_series(df)
+                    title = "CCN Histogram"
+                    # fix awkward scaling if CCN=1 greatly outweighs other values
+                    if X[0] == 1 and Y[0] - max(Y[1:]) > 15:
+                        X, Y = X[1:], Y[1:]
+                        title += " (without CCN=1)"
+                    with dpg.plot(label=title, height=-1, width=-1):
+                        x_tag = f"{file}__x_axis"
+                        y_tag = f"{file}__y_axis"
+
+                        dpg.add_plot_axis(dpg.mvXAxis, label='CCN', tag=x_tag, no_gridlines=True)
+                        x_max = int(np.max(X)) + 1
+                        x_tick_step = math.ceil((x_max - 1) / 10)
+                        dpg.set_axis_ticks(dpg.last_item(), tuple((str(x), x) for x in range(1, x_max, x_tick_step)))
+                        dpg.set_axis_limits(x_tag, np.min(X)-1, np.max(X)+1)
+                        dpg.add_plot_axis(dpg.mvYAxis, label='Count', tag=y_tag)
+
+                        y_max = np.max(Y) + 1
+                        y_tick_step = math.ceil((y_max - 1) / 4)
+                        dpg.set_axis_ticks(dpg.last_item(), tuple((str(x), x) for x in range(0, y_max, y_tick_step)))
+                        dpg.set_axis_limits(y_tag, 0, y_max)
+
+                        dpg.add_bar_series(X, Y, parent=dpg.last_item(), weight=0.5)
+
+    def make_histogram_series(df):
+        uniq_values = len(df.groupby(['CCN']))
+        num_bins = min(uniq_values, 8)
+        labels = list(range(num_bins))
+        bars, bins = pd.cut(df.get('CCN'), num_bins, labels=labels, retbins=True)
+        bins = bins[:-1]
+        bins[0] = math.ceil(bins[0])
+        bins[-1] = math.ceil(bins[-1])
+        bins = np.floor(bins)
+        ret = [0] * num_bins
+        for x in list(bars):
+            ret[x] += 1
+        return bins, ret
+
 
     def write_table(table_id, df, columns: dict[str, ColumnInfo]):
         dpg.delete_item(table_id, children_only=True)
